7ecf10b04d43bee1e30e70ef75ecce7d20f6cb61
wpbest <wpbest@gmail.com>
2016-10-11 10:41:35 -0600

    Initial commit

---
+++ src/dotnetstarter/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+public class Startup
+{
+	public void Configure(IApplicationBuilder app)
+	{
+		app.UseDefaultFiles();
+		app.UseStaticFiles();
+    }
+
+    public static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+        var host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseConfiguration(config)
+		    .UseUrls("http://*:5000")
+                    .UseStartup<Startup>()
+                    .Build();
+        host.Run();
+    }
+}
